36f19c56f05e5f01b31d9ae4f856231ba6d45ce1
Mirak005 <karimgharbbi@gmail.com>
2021-07-04 23:50:47 +0100

    add alert to the pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,61 +1,64 @@
-import { BrowserRouter as Router, Route} from 'react-router-dom'
-import {Container} from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
-import AboutusScreen from './screens/AboutusScreen'
-import NewsScreen from './screens/NewsScreen'
-import CartScreen from './screens/CartScreen'
-import ProductsScreen from './screens/ProductsScreen'
-import LoginScreen from './screens/LoginScreen'
-import ProfileScreen from './screens/ProfileScreen'
-import UserListScreen from './screens/UserListScreen' 
-import RegisterScreen from './screens/RegisterScreen'
-import UserEditScreen from './screens/UserEditScreen'
-import ProductListScreen from './screens/ProductListScreen'
-import ProductEditScreen from './screens/ProductEditScreen'
-import NewsListScreen from './screens/NewsListScreen'
-import NewsEditScreen from './screens/NewsEditScreen'
-import ContactScreen from './screens/ContactScreen'
-import ShippingScreen from './screens/ShippingScreen'
-import PlaceOrderScreen from './screens/PlaceOrderScreen'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import AboutusScreen from "./screens/AboutusScreen";
+import NewsScreen from "./screens/NewsScreen";
+import CartScreen from "./screens/CartScreen";
+import ProductsScreen from "./screens/ProductsScreen";
+import LoginScreen from "./screens/LoginScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import UserListScreen from "./screens/UserListScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import UserEditScreen from "./screens/UserEditScreen";
+import ProductListScreen from "./screens/ProductListScreen";
+import ProductEditScreen from "./screens/ProductEditScreen";
+import NewsListScreen from "./screens/NewsListScreen";
+import NewsEditScreen from "./screens/NewsEditScreen";
+import ContactScreen from "./screens/ContactScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import AlertComponent from "./components/AlertComponent";
 
-const App =()  => {
+const App = () => {
   return (
     <>
-    <Router>
-    <Header/>
-    <main className='py-3'>
-      <Container>
-        <Route path='/' component={HomeScreen} exact/>
-        <Route path='/product/:id' component={ProductScreen} exact/>
-        <Route path='/search/:keyword' component={ProductsScreen} exact/>
-        <Route path='/shipping' component={ShippingScreen}/>
-        <Route path='/login' component={LoginScreen} exact/>
-        <Route path='/register' component={RegisterScreen} exact/>
-        <Route path='/news/:id' component={NewsScreen} exact />
-        <Route path='/admin/newslist' component={NewsListScreen}/>
-        <Route path='/admin/news/:id/edit' component={NewsEditScreen}/>
-        <Route path='/about' component={AboutusScreen} exact />
-        <Route path='/contact' component={ContactScreen} exact />
-        <Route path='/cart/:id?' component={CartScreen}/>
-        <Route path='/admin/userlist' component={UserListScreen}/>
-        <Route path='/admin/user/:id/edit' component={UserEditScreen}/>
-        <Route path='/admin/productlist' component={ProductListScreen}/>
-        <Route path='/admin/product/:id/edit' component={ProductEditScreen}/>
-        <Route path='/ourproducts'component={ProductsScreen} exact/>
-        <Route path='/profile'component={ProfileScreen} exact/>
-        <Route path='/placeorder'component={PlaceOrderScreen} exact/>
-
-    
-      </Container>
-    
-    </main>
-    <Footer/>
-    </Router>
+      <AlertComponent />
+      <Router>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Route path="/" component={HomeScreen} exact />
+            <Route path="/product/:id" component={ProductScreen} exact />
+            <Route path="/search/:keyword" component={ProductsScreen} exact />
+            <Route path="/shipping" component={ShippingScreen} />
+            <Route path="/login" component={LoginScreen} exact />
+            <Route path="/register" component={RegisterScreen} exact />
+            <Route path="/news/:id" component={NewsScreen} exact />
+            <Route path="/admin/newslist" component={NewsListScreen} />
+            <Route path="/admin/news/:id/edit" component={NewsEditScreen} />
+            <Route path="/about" component={AboutusScreen} exact />
+            <Route path="/contact" component={ContactScreen} exact />
+            <Route path="/cart/:id?" component={CartScreen} />
+            <Route path="/admin/userlist" component={UserListScreen} />
+            <Route path="/admin/user/:id/edit" component={UserEditScreen} />
+            <Route path="/admin/productlist" component={ProductListScreen} />
+            <Route
+              path="/admin/product/:id/edit"
+              component={ProductEditScreen}
+            />
+            <Route path="/ourproducts" component={ProductsScreen} exact />
+            <Route path="/profile" component={ProfileScreen} exact />
+            <Route path="/placeorder" component={PlaceOrderScreen} exact />
+          </Container>
+        </main>
+        <Footer />
+      </Router>
     </>
   );
-}
+};
 
 export default App;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,50 +1,80 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import {productListReducer,productDetailsReducer, productDeleteReducer, productCreateReducer, productUpdateReducer} from './reducers/productReducers'
-import { cartReducer} from './reducers/cartReducers'
-import {newsListReducer,newsDetailsReducer,newsDeleteReducer,newsCreateReducer,newsUpdateReducer} from './reducers/newsReducers'
-import {userLoginReducer,userRegisterReducer, userDetailsReducer, userUpdateProfileReducer,userListReducer, userDeleteReducer,userUpdateReducer} from './reducers/userReducers'
-
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import {
+  productListReducer,
+  productDetailsReducer,
+  productDeleteReducer,
+  productCreateReducer,
+  productUpdateReducer,
+} from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
+import {
+  newsListReducer,
+  newsDetailsReducer,
+  newsDeleteReducer,
+  newsCreateReducer,
+  newsUpdateReducer,
+} from "./reducers/newsReducers";
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userDetailsReducer,
+  userUpdateProfileReducer,
+  userListReducer,
+  userDeleteReducer,
+  userUpdateReducer,
+} from "./reducers/userReducers";
+import { alertReducer } from "./reducers/alertReducer";
 
 const reducer = combineReducers({
-    productList:productListReducer,
-    productDetails:productDetailsReducer,
-    productDelete:productDeleteReducer,
-    productCreate:productCreateReducer,
-    productUpdate:productUpdateReducer,
-    cart:cartReducer,
-    newsList:newsListReducer,
-    newsDetails:newsDetailsReducer,
-    newsDelete:newsDeleteReducer,
-    newsCreate:newsCreateReducer,
-    newsUpdate:newsUpdateReducer,
-    userLogin:userLoginReducer,
-    userRegister:userRegisterReducer,
-    userDetails:userDetailsReducer,
-    userUpdateProfile:userUpdateProfileReducer,
-    userList:userListReducer,
-    userDelete:userDeleteReducer,
-    userUpdate:userUpdateReducer,
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  productDelete: productDeleteReducer,
+  productCreate: productCreateReducer,
+  productUpdate: productUpdateReducer,
+  cart: cartReducer,
+  newsList: newsListReducer,
+  newsDetails: newsDetailsReducer,
+  newsDelete: newsDeleteReducer,
+  newsCreate: newsCreateReducer,
+  newsUpdate: newsUpdateReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
+  userList: userListReducer,
+  userDelete: userDeleteReducer,
+  userUpdate: userUpdateReducer,
+  alertReducer,
+});
 
-})
+const cartItemFromStorage = localStorage.getItem("cartItem")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
 
-const cartItemFromStorage = localStorage.getItem('cartItem')? JSON.parse(localStorage.getItem('cartItems')):[]
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
 
-const userInfoFromStorage = localStorage.getItem('userInfo')? JSON.parse(localStorage.getItem('userInfo')):null
-
-const shippingAddressFromStorage = localStorage.getItem('shippingAddress')? JSON.parse(localStorage.getItem('shippingAddress')):{}
+const shippingAddressFromStorage = localStorage.getItem("shippingAddress")
+  ? JSON.parse(localStorage.getItem("shippingAddress"))
+  : {};
 
 const initialState = {
-    cart:{cartItems:cartItemFromStorage,shippingAddressFromStorage:shippingAddressFromStorage},
-    userLogin:{userInfo:userInfoFromStorage}
-}
+  cart: {
+    cartItems: cartItemFromStorage,
+    shippingAddressFromStorage: shippingAddressFromStorage,
+  },
+  userLogin: { userInfo: userInfoFromStorage },
+};
 
-const middleware=[thunk]
+const middleware = [thunk];
 
 const store = createStore(
-    reducer,
-    initialState,
-    composeWithDevTools(applyMiddleware(...middleware)))
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
-export default store
+export default store;
